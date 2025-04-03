abf72fab4e8d6dd332cb4bfb9a382346fda0c50d
Koichi Ito <kito@j-code.org>
2025-04-03 23:18:33 +0900

    v0.1.5

--- src/gui/lib/libraries/extensions/entry/index-stretch3.jsx
+++ src/gui/lib/libraries/extensions/entry/index-stretch3.jsx
@@ -10,7 +10,7 @@ import pcratchIoTConnectionIconURL from './connection-icon.svg';
 import pcratchIoTConnectionSmallIconURL from './connection-small-icon.svg';
 import translations from './translations.json';
 
-const version = 'v0.1.2';
+const version = 'v0.1.5';
 
 const entry = {
     get name () {
